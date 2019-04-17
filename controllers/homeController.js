439ee521a8a0a6ff76922048977f09c807a9e694
Muhammad Farhan <itianz@outlook.com>
2019-04-18 03:13:14 +0500

    encapsulate to module.exports

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,22 +1,22 @@
 module.exports = {
 	index: function(req, res) {
-	    var data={};
+		var data={};
 		var tagline = "Any code of your own that you haven't looked.";
 		var drinks = [
-	        { name: 'Bloody Mary', drunkness: 3 },
-	        { name: 'Martini', drunkness: 5 },
-	        { name: 'Charli', drunkness: 10 }
+			{ name: 'Bloody Mary', drunkness: 3 },
+			{ name: 'Martini', drunkness: 5 },
+			{ name: 'Charli', drunkness: 10 }
 	    ];
-	    data.drinks=drinks;
-	    data.tagline=tagline;
-	    data.content_view='index';
-	    data.title='node app skeleton';
+		data.drinks=drinks;
+		data.tagline=tagline;
+		data.content_view='index';
+		data.title='node app skeleton';
 		res.render('content', data);
 	},
 	page2: function(req, res){
 		var data={};
 		data.content_view='page2';
-	    data.title='node app skeleton';
+		data.title='node app skeleton';
 		res.render('content', data);
 	}
 }
